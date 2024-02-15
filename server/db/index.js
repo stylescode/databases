674a42ea30ad2e074fb851e9653a9cf04a939bc9
Styles <stylesk11@gmail.com>
2024-02-14 17:32:21 -0800

    pair work Complete customizing tests

--- server/db/index.js
+++ server/db/index.js
@@ -1,7 +1,16 @@
 var mysql = require('mysql2');
 
 // Create a database connection and export it from this file.
+exports.dbConnection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  database: 'chat'
+});
 // Confirm that the credentials supplied for the connection are correct.
+
+dbConnection.connect();
+
+
 // On your personal computer supply the correct credentials for your mySQL account -- likely
 // user: 'root', password: ''
 // OR
--- server/models/messages.js
+++ server/models/messages.js
@@ -1,6 +1,22 @@
-var db = require('../db');
+var { dbConnection } = require('../db');
 
 module.exports = {
-  getAll: function () {}, // a function which produces all the messages
+  getAll: function () {
+    // establish connection
+    dbConnection.connect();
+    // create promise that represents submitted query
+    dbConnection.query(
+      `SELECT
+      messages.user_message,
+      messages.created_by
+      FROM messages
+      INNER JOIN rooms ON messages.room = rooms.id
+      INNER JOIN users ON messages.created_by = users.id`
+    );
+
+  }, // a function which produces all the messages
+
+
+
   create: function () {} // a function which can be used to insert a message into the database
 };
